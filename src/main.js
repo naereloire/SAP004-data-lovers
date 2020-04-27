304ed2ca4e808162638280201384f85e48b53c5d
Camila Cunha <millagerarde@gmail.com>
2020-04-26 23:23:26 -0300

    alterações do código

--- src/main.js
+++ src/main.js
@@ -6,3 +6,6 @@ const pokemon = data.pokemon[0].name;
 console.log(pokemon);
 const pokemonImg = data.pokemon[0].img;
 console.log(pokemonImg);
+
+const pokemonImg2 = data.pokemon[0].img;
+document.getElementById("cards").innerHTML = pokemonImg2;
