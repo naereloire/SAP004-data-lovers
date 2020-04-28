687ed893d82ab129f40e59111898676768af2caa
naereloire <61746290+naereloire@users.noreply.github.com>
2020-04-28 15:52:30 -0300

    Revert "Show pokemons"

--- src/data.js
+++ src/data.js
@@ -1,23 +1,25 @@
+
+
 import data from './data/pokemon/pokemon.js';
 
-function getData() {
-  let listPokemon = data.pokemon
-  let newListCard = []
-  for (let pokemon of listPokemon) {
-    let infosCard = {
-      numero: pokemon.num,
-      name: pokemon.name,
-      imagem: pokemon.img
-    }
-    newListCard.push(infosCard)
-  }
-  // debugger
-  return newListCard
+function getData(){
+let listPokemon= data.pokemon
+let newListCard=[]
+for (let pokemon of listPokemon){
+  let infosCard={numero:pokemon.num,name:pokemon.name,imagem:pokemon.img}
+  newListCard.push(infosCard)
+}
+// debugger
+return newListCard
+
+
+
 }
 
 
-const dataManipulation = {
-  getData: getData
+const dataManipulation= {
+  getData:getData
+
 }
 
 export default dataManipulation;
