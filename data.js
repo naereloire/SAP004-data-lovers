16f9fdb9234a25b9ee6f3b4d645f5f50141d4e38
naereloire <naere.loire@gmail.com>
2020-04-28 19:59:55 -0300

    Updates

---
+++ data.js
@@ -0,0 +1,20 @@
+export function getData(data){
+let listPokemon= data.pokemon
+let newListCard=[]
+for (let pokemon of listPokemon){
+  let infosCard={
+    
+    numero:pokemon.num,
+    name:pokemon.name,
+    imagem:pokemon.img}
+  
+  newListCard.push(infosCard)
+}
+// debugger
+return newListCard
+
+
+
+}
+
+
---
+++ main.js
@@ -0,0 +1,19 @@
+import data from './data/pokemon/pokemon.js';
+import {getData} from './data.js'
+
+
+let arrayPokemon=getData(data)
+let getDivCards=document.getElementById("local-cards")
+
+for(let pokemon of arrayPokemon){
+let createDivCard=document.createElement("div")
+createDivCard.className="card-style"
+createDivCard.innerHTML+=pokemon.name + "<br>" + pokemon.numero
+getDivCards.appendChild(createDivCard)
+let createImgPokemon=document.createElement("img")
+createDivCard.appendChild(createImgPokemon)
+createImgPokemon.srcset=pokemon.imagem
+
+
+ 
+}
