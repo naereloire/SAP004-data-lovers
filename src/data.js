df8d790344119c5fd54d1b5f3a10924da379e498
naereloire <naere.loire@gmail.com>
2020-05-01 20:00:00 -0300

    implementada funcao ordenacao por nome e numero

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
-export function getData(data){
-  let listPokemon= data.pokemon
+export function selectInfosToShow(data){
+  let listPokemon= data
   let newListCard=[]
   for (let pokemon of listPokemon){
     let infosCard={
@@ -10,6 +10,44 @@ export function getData(data){
     
     newListCard.push(infosCard)
   }
-  // debugger
+  
   return newListCard
   }
+
+  function alphaCrescent(objeto1,objeto2,option){
+    
+    if (objeto1[option]<objeto2[option]){
+      return -1
+    }
+    if (objeto1[option]>objeto2[option]){
+    return 1
+    }
+    return 0
+  }
+
+  function alphaDecreasing(objeto1,objeto2,option){
+    if (objeto1[option]<objeto2[option]){
+      return 1
+    }
+    if (objeto1[option]>objeto2[option]){
+    return -1
+    }
+    return 0
+  }
+  
+  
+  
+  export function ordenation(data,option,order){
+    let listPokemon= data
+    let sortedList=[]
+    if (order=="increasing"){
+      sortedList=listPokemon.sort(function(a,b){return alphaCrescent(a,b,option)} )
+    }
+    if (order=="decreasing"){
+      sortedList=listPokemon.sort(function(a,b){return alphaDecreasing(a,b,option)} )
+    }
+    return sortedList
+
+  }
+  // mostrar + e 
+  // esplelhar cards
--- src/main.js
+++ src/main.js
@@ -1,15 +1,54 @@
 import data from './data/pokemon/pokemon.js';
-import {getData} from './data.js'
+import {selectInfosToShow, ordenation } from './data.js'
 
-let arrayPokemon=getData(data)
-let getDivCards=document.getElementById("local-cards")
+let arrayPokemon = data.pokemon
 
-for(let pokemon of arrayPokemon){
-let createDivCard=document.createElement("div")
-createDivCard.className="card-style"
-createDivCard.innerHTML+=pokemon.name + "<br>" + pokemon.numero
-getDivCards.appendChild(createDivCard)
-let createImgPokemon=document.createElement("img")
-createDivCard.appendChild(createImgPokemon)
-createImgPokemon.srcset=pokemon.imagem
+
+function showPokemons(arrayPokemon) {
+    arrayPokemon = selectInfosToShow(arrayPokemon)
+
+    let getDivCards = document.getElementById("local-cards")
+    getDivCards.innerHTML = ""
+
+    for (let pokemon of arrayPokemon) {
+        let createDivCard = document.createElement("div")
+        createDivCard.className = "card-style"
+        createDivCard.innerHTML += pokemon.name + "<br>" + pokemon.numero
+        getDivCards.appendChild(createDivCard)
+        let createImgPokemon = document.createElement("img")
+        createDivCard.appendChild(createImgPokemon)
+        createImgPokemon.srcset = pokemon.imagem
+    }
 }
+showPokemons(arrayPokemon)
+
+let getSelectOrder = document.getElementById("ordination")
+
+
+function sortPokemons(event) {
+    // debugger;
+    let elementoSelect = event.target
+    let selectedOption = elementoSelect.options[elementoSelect.selectedIndex].value
+    let list = []
+    if (selectedOption == "a-z") {
+        list = ordenation(arrayPokemon, "name", "increasing")
+    }
+    if (selectedOption == "z-a") {
+        list = ordenation(arrayPokemon, "name", "decreasing")
+    }
+
+    if (selectedOption=="num-cresc") {
+        list = ordenation(arrayPokemon,"num","increasing")
+    }
+    if (selectedOption=="num-decresc") {
+        list = ordenation(arrayPokemon,"num","decreasing")
+    }
+    debugger;
+    showPokemons(list)
+}
+
+
+
+getSelectOrder.addEventListener("change", sortPokemons)
+
+// refatorar com método split
