40f7aac729f620c21278f18fbfcef15252b2576a
Camila Cunha <millagerarde@gmail.com>
2020-05-02 17:59:21 -0300

    Inclusão de tipos e fraquezas no objeto infosCard

--- src/data.js
+++ src/data.js
@@ -1,14 +1,15 @@
-export function getData(data){
-  let listPokemon= data.pokemon
-  let newListCard=[]
-  for (let pokemon of listPokemon){
-    let infosCard={      
-      numero:pokemon.num,
-      name:pokemon.name,
-      imagem:pokemon.img}
-    
-    newListCard.push(infosCard)
-  }
-  // debugger
-  return newListCard
+export function getData(data) {
+    let listPokemon = data.pokemon;
+    let newListCard = [];
+    for (let pokemon of listPokemon) {
+      let infosCard = {
+        numero: pokemon.num,
+        nome: pokemon.name,
+        imagem: pokemon.img,
+        tipos: pokemon.type.join(", "),
+        fraquezas: pokemon.weaknesses.join(", ")
+      }
+      newListCard.push(infosCard);
+    }
+    return newListCard;
   }
