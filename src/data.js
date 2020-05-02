bdb94263e6961130db9b913ec3077f19f78e9e9a
naereloire <naere.loire@gmail.com>
2020-05-02 16:08:51 -0300

    implementada funcao filterPokemons no select tipos

--- src/data.js
+++ src/data.js
@@ -1,55 +1,79 @@
-export function selectInfosToShow(data){
-  let listPokemon= data
-  let newListCard=[]
-  for (let pokemon of listPokemon){
-    let infosCard={
-      
-      number:pokemon.num,
-      name:pokemon.name,
-      image:pokemon.img,
-      probability:pokemon.spawn_chance
+export function selectInfosToShow(data) {
+  let listPokemon = data
+  let newListCard = []
+  for (let pokemon of listPokemon) {
+    let infosCard = {
+
+      number: pokemon.num,
+      name: pokemon.name,
+      image: pokemon.img,
+      probability: pokemon.spawn_chance
     }
-    
+
     newListCard.push(infosCard)
   }
-  
+
   return newListCard
-  }
+}
 
-  function sortCrescent(objeto1,objeto2,option){
-    
-    if (objeto1[option]<objeto2[option]){
-      return -1
-    }
-    if (objeto1[option]>objeto2[option]){
-    return 1
-    }
-    return 0
-  }
+function sortCrescent(objeto1, objeto2, option) {
 
-  function sortDecreasing(objeto1,objeto2,option){
-    if (objeto1[option]<objeto2[option]){
-      return 1
-    }
-    if (objeto1[option]>objeto2[option]){
+  if (objeto1[option] < objeto2[option]) {
     return -1
-    }
-    return 0
   }
-  
-  
-  
-  export function ordenation(data,option,order){
-    let listPokemon= data
-    let sortedList=[]
-    if (order=="increasing"){
-      sortedList=listPokemon.sort(function(a,b){return sortCrescent(a,b,option)} )
+  if (objeto1[option] > objeto2[option]) {
+    return 1
+  }
+  return 0
+}
+
+function sortDecreasing(objeto1, objeto2, option) {
+  if (objeto1[option] < objeto2[option]) {
+    return 1
+  }
+  if (objeto1[option] > objeto2[option]) {
+    return -1
+  }
+  return 0
+}
+
+
+
+export function ordenation(data, option, order) {
+  let listPokemon = data
+  let sortedList = []
+  if (order == "increasing") {
+    sortedList = listPokemon.sort(function (a, b) { return sortCrescent(a, b, option) })
+  }
+  if (order == "decreasing") {
+    sortedList = listPokemon.sort(function (a, b) { return sortDecreasing(a, b, option) })
+  }
+  return sortedList
+
+}
+
+function compareCategory(objeto, option, category) {
+  if (Array.isArray(objeto[option])){
+    for (let element of objeto[option]) {
+      if (element == category) {
+        return true
+      }
+      else {
+        return objeto[option] == category
+      }
     }
-    if (order=="decreasing"){
-      sortedList=listPokemon.sort(function(a,b){return sortDecreasing(a,b,option)} )
-    }
-    return sortedList
 
   }
-  // mostrar + e 
+
+
+}
+
+export function filterInfons(data, option, category) {
+  let listPokemon = data
+  let filteredList = []
+  filteredList=listPokemon.filter(function(x){return compareCategory(x,option,category)})
+  return filteredList
+}
+
+  // mostrar + e
   // esplelhar cards
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import data from './data/pokemon/pokemon.js';
-import {selectInfosToShow, ordenation } from './data.js'
+import {selectInfosToShow, ordenation, filterInfons } from './data.js'
 
 let arrayPokemon = data.pokemon
 
@@ -35,8 +35,8 @@ let getSelectOrder = document.getElementById("ordination")
 
 function sortPokemons(event) {
     // debugger;
-    let elementoSelect = event.target
-    let selectedOption = elementoSelect.options[elementoSelect.selectedIndex].value
+    let elementSelect = event.target
+    let selectedOption = elementSelect.options[elementSelect.selectedIndex].value
     let list = []
     if(selectedOption==""){list=arrayPokemon}
     
@@ -45,22 +45,24 @@ function sortPokemons(event) {
     list=ordenation(arrayPokemon,arrayParameters[0],arrayParameters[1])
 
     }
-    // refatorado com método split
-    // if (selectedOption == "a-z") {
-    //     list = ordenation(arrayPokemon, "name", "increasing")
-    // }
-    // if (selectedOption == "z-a") {
-    //     list = ordenation(arrayPokemon, "name", "decreasing")
-    // }
-
-    // if (selectedOption=="num-cresc") {
-    //     list = ordenation(arrayPokemon,"num","increasing")
-    // }
-    // if (selectedOption=="num-decresc") {
-    //     list = ordenation(arrayPokemon,"num","decreasing")
-    // }
-    // debugger;
     showPokemons(list)
 }
 getSelectOrder.addEventListener("change", sortPokemons)
 
+
+let getSelectFilter = document.getElementById("filter")
+
+function filterPokemons (event){
+let elementSelect=event.target
+let selectedOption=elementSelect.options[elementSelect.selectedIndex].value    
+let list=[]
+if (selectedOption==""){list=arrayPokemon}
+else{
+let arrayParameters=selectedOption.split("-")  
+list=filterInfons(arrayPokemon,arrayParameters[0],arrayParameters[1])
+}
+
+showPokemons(list)
+}
+
+getSelectFilter.addEventListener("change", filterPokemons)
