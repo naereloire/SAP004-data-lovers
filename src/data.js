27e5a4596ae1ad2b63b6d6ca60fc803c0ca62c60
Camila Cunha <millagerarde@gmail.com>
2020-05-04 13:45:00 -0300

    atualização da função no data de acordo com master

--- src/data.js
+++ src/data.js
@@ -1,15 +1,71 @@
-export function getData(data) {
-    let listPokemon = data.pokemon;
+  export function selectInfosToShow(data) {
+    let listPokemon = data;
     let newListCard = [];
     for (let pokemon of listPokemon) {
       let infosCard = {
-        numero: pokemon.num,
-        nome: pokemon.name,
-        imagem: pokemon.img,
-        tipos: pokemon.type.join(", "),
-        fraquezas: pokemon.weaknesses.join(", ")
+        number: pokemon.num,
+        name: pokemon.name,
+        image: pokemon.img,
+        probability: pokemon.spawn_chance
+        types: pokemon.type.join(", "),
+        weaknesses: pokemon.weaknesses.join(", ")
       }
-      newListCard.push(infosCard);
+      newListCard.push(infosCard)
     }
-    return newListCard;
+    return newListCard
   }
+  
+  function sortCrescent(objeto1, objeto2, option) {
+    if (objeto1[option] < objeto2[option]) {
+      return -1
+    }
+    if (objeto1[option] > objeto2[option]) {
+      return 1
+    }
+    return 0
+  }
+  
+  function sortDecreasing(objeto1, objeto2, option) {
+    if (objeto1[option] < objeto2[option]) {
+      return 1
+    }
+    if (objeto1[option] > objeto2[option]) {
+      return -1
+    }
+    return 0
+  }
+  
+  export function ordenation(data, option, order) {
+    let listPokemon = data
+    let sortedList = []
+    if (order == "increasing") {
+      sortedList = listPokemon.sort(function (a, b) { return sortCrescent(a, b, option) })
+    }
+    if (order == "decreasing") {
+      sortedList = listPokemon.sort(function (a, b) { return sortDecreasing(a, b, option) })
+    }
+    return sortedList
+  }
+  
+  function compareCategory(objeto, option, category) {
+    if (Array.isArray(objeto[option])) {
+      for (let element of objeto[option]) {
+        if (element == category) {
+          return true
+        }
+      }
+    }
+    else {
+      return objeto[option] == category
+    }
+  }
+  
+  export function filterInfons(data, option, category) {
+    let listPokemon = data
+    let filteredList = []
+    filteredList = listPokemon.filter(function (x) { return compareCategory(x, option, category) })
+    return filteredList
+  }
+  
+    // mostrar + e
+    // esplelhar cards
