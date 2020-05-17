f60a7740a276498e9a5a00a899d25cacac0c1575
naereloire <naere.loire@gmail.com>
2020-05-17 15:06:13 -0300

    identacao

--- src/compute.js
+++ src/compute.js
@@ -1,5 +1,5 @@
 import data from './data/pokemon/pokemon.js';
-import { ordenation, computeCp, filterInfons } from './data.js'
+import { ordenation, computeCp, filterInfons, getNextEvolution } from './data.js'
 
 const arrayPokemon = data.pokemon;
 let dataListSorted = ordenation(arrayPokemon, "name", "increasing");
@@ -25,23 +25,49 @@ function calculateAndShow(event) {
   if (inputCp === "") { inputCp = 0 }
   let result = (computeCp(arrayPokemon, inputCp, inputName));
   let pokemon = filterInfons(arrayPokemon, "name", inputName);
+  let evolutions = getNextEvolution(arrayPokemon, inputName)
+
   let card = "";
   showPokemon.innerHTML = "";
   pokemon = pokemon[0];
 
-  if (inputName !== "" && inputCp !== "") {
+  if (evolutions.length === 0) {
     card += `
-          <div class="card-style">
-            <h1>${pokemon.name}</h1>
-            <img class="img-poke" src ="${pokemon.img}" alt ="imagem ${pokemon.name}"/>
-          </div>
-          <div class="card-style">
-            <p><strong>Média CP:</strong> ${result.mediaCp}<br>
-            <strong>Min CP:</strong> ${result.minCp}<br>
-            <strong>Max CP:</strong> ${result.maxCp}</p>
-            <p><strong>Doce:</strong> ${pokemon.candy}<br>
-            <strong>Quant. de doces:</strong> ${pokemon.candy_count}</p>
-          </div>`;
+  <div class="card-style">
+   <h1>${pokemon.name}</h1>
+    <img class="img-poke" src ="${pokemon.img}" alt ="imagem ${pokemon.name}"/>
+  </div>
+  
+  <div class="card-style">
+   <strong>Este pokemon não possui evolução.</strong>
+  </div>`;
+    showPokemon.innerHTML = card;
   }
-  showPokemon.innerHTML = card;
+  else {
+    if (inputName !== "" && inputCp !== "") {
+    card += `
+    <div class="card-style">
+     <h1>${pokemon.name}</h1>
+     <img class="img-poke" src ="${pokemon.img}" alt ="imagem ${pokemon.name}"/>
+    </div>
+          
+    <div class="div-seta">
+      <img class="seta-poke" src ="https://lh3.googleusercontent.com/proxy/txb6TxVeHpioJKExAgQNM5uzO5mZDhG0nlD3NccPbHnyHYUmdqrYbZvxANGWLlhtrMaZ4LQU5DRsLAjizS5qi1_vokYn7-aNlrXKJKBk8hX6efozJCr_" alt ="imagem seta"/>
+    </div>
+
+    <div class="card-style">
+      <h1>${evolutions[0].name}</h1>
+      <img class="img-poke" src ="${evolutions[0].img}" alt ="imagem ${evolutions[0].name}"/>
+    </div>
+          
+    <div class="card-style">
+      <p><strong>Média CP:</strong> ${result.mediaCp}<br>
+      <strong>Min CP:</strong> ${result.minCp}<br>
+      <strong>Max CP:</strong> ${result.maxCp}</p>
+      <p><strong>Doce:</strong> ${pokemon.candy}<br>
+      <strong>Quant. de doces:</strong> ${pokemon.candy_count}</p>
+    </div>`;
+    }
+    showPokemon.innerHTML = card;
+  }
 }
